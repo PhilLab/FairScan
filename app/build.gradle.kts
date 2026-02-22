6d52442e471d7d84126309b8ee5b70ab8ad05671
Philipp Hasper <vcs@hasper.info>
2026-02-22 08:46:29 +0100

    Debug build variant with different applicationId and launcher icon
    This makes it more convenient to develop on your device when otherwise
    using the app productively:
    Your development build will not run into signing errors, will not
    overwrite the productive app, and you can distinguish them by the debug
    variant having a "DEV" text written under the icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,9 @@ android {
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+        }
         release {
             isMinifyEnabled = true
             isShrinkResources = true
